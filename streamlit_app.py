38d9b1e5316ca4608ffc1e2fa46659a68388cbd8
anouk1222 <116896259+anouk1222@users.noreply.github.com>
2023-05-24 17:55:04 +0200

    Update streamlit_app.py
    add smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,10 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Bluebery Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled free range egg')
+streamlit.header('Breakfast favorites')
+streamlit.text('🥣 Omega 3 & Bluebery Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled free range egg')
+streamlit.text('🥑🍞 Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
